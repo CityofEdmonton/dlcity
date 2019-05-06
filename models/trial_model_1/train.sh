a6b04fd14819160bf6e3dfbae17e79e5f03d9747
Liyao Jiang <liyao.jiang@edmonton.ca>
2019-05-06 14:40:59 -0600

    update path for run in vm

--- models/trial_model_1/train.sh
+++ models/trial_model_1/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-caffe train --solver=/home/liyao_jiang/dlcity/models/trial_model_1/prototxt/solver.prototxt \
---weights /home/liyao_jiang/dlcity/models/VGG_ILSVRC_19_layers.caffemodel \
-2>&1 | tee /home/liyao_jiang/dlcity/models/trial_model_1/logs/trial_model_1.log
+caffe train --solver=/home/aimladmin/liyao_workspace/dlcity/models/trial_model_1/prototxt/solver.prototxt \
+--weights /home/aimladmin/liyao_workspace/dlcity/models/VGG_ILSVRC_19_layers.caffemodel \
+2>&1 | tee /home/aimladmin/liyao_workspace/dlcity/models/trial_model_1/logs/trial_model_1.log
