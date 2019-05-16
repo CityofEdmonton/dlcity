7c581701fbd42afc8b6178ec09d40fd4aa70c745
Liyao Jiang <liyao.jiang@edmonton.ca>
2019-05-16 13:23:09 -0600

    script for creating lmdb, then get meanfile

---
+++ yeg_src/create_lmdb.py
@@ -0,0 +1,55 @@
+import os
+import glob
+import random
+import numpy as np
+
+import cv2
+
+import caffe
+from caffe.proto import caffe_pb2
+import lmdb
+
+#Size of images
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
+def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
+
+    #Histogram Equalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+
+    #Image Resizing
+    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+
+    return img
+
+
+def make_datum(img, label):
+    #image is numpy.ndarray format. BGR instead of RGB
+    return caffe_pb2.Datum(
+        channels=3,
+        width=IMAGE_WIDTH,
+        height=IMAGE_HEIGHT,
+        label=label,
+        data=np.rollaxis(img, 2).tostring())
+
+train_lmdb = r'/home/aimladmin/liyao_workspace/dlcity/yeg_data/lmdb'
+
+train_data = [img for img in glob.glob(r"/home/aimladmin/liyao_workspace/dlcity/yeg_data/images/*.jpg")]
+
+print ('Creating train_lmdb')
+
+in_db = lmdb.open(train_lmdb, map_size=int(1e12))
+with in_db.begin(write=True) as in_txn:
+    for in_idx, img_path in enumerate(train_data):
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
+        label = 0
+        datum = make_datum(img, label)
+        in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
+        print ('{:0>5d}'.format(in_idx) + ':' + img_path)
+in_db.close()
+
+print ('\nFinished processing all images')
