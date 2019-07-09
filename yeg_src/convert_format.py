68579dc4931d2948140b54695fc81fdb87932e7a
Liyao Jiang <liyao.jiang@edmonton.ca>
2019-07-09 08:50:00 -0600

    change the logic for finding invalid images

--- yeg_src/convert_format.py
+++ yeg_src/convert_format.py
@@ -28,19 +28,19 @@ def parse_train_data():
     new_df['r_url'] = 'url'
     new_df.to_csv(r'H:\workspace\dlcity\yeg_data\train_safe_parsed_emptyurl.csv', index=False, sep=',')
 
-def is_valid(row, invalid_list):
+def is_valid(row, valid_list):
     left = str(row['left'])
     right = str(row['right'])  
-    if (left in invalid_list) or (right in invalid_list):
-        return False
-    else:
+    if (left in valid_list) and (right in valid_list):
         return True
+    else:
+        return False
 
 def cleanup_invalid_images():
     filename =  r'H:\workspace\dlcity\yeg_data\train_safe_parsed_emptyurl.csv'
     df = pandas.read_csv(filename)
-    invalid_list = os.listdir(r'H:\workspace\dlcity\yeg_data\images_invalid')
-    df['is_valid'] = df.apply(lambda row : is_valid(row, invalid_list), axis=1)
+    valid_list = os.listdir(r'H:\workspace\dlcity\yeg_data\images')
+    df['is_valid'] = df.apply(lambda row : is_valid(row, valid_list), axis=1)
     df = df[df.is_valid == True].drop(labels='is_valid', axis=1)
     df.to_csv(r'H:\workspace\dlcity\yeg_data\train_safe_parsed_emptyurl.csv', index=False, sep=',')
 
